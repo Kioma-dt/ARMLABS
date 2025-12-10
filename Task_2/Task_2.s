c98ac3459ea17eaa868e3782ba595a1ffd7512f1
Avramenko Roman <www.kioma220@gmail.com>
2025-12-10 20:59:07 +0300

    Task_2

--- Task_2/Task_2.s
+++ Task_2/Task_2.s
@@ -1,7 +1,7 @@
 .section .bss
 input_buffer:   .skip 64
 output_buffer:  .skip 64
-matrix:      .skip 128
+matrix: .skip 64
 matrix_size: .skip 2
 
 .section .text
@@ -63,14 +63,93 @@ good_input_size:
     b _start
 
 
+
+good_input:
+
+// x21 - end
+// x20 - matrix size
+// x19 - matrix_ptr
+// x6 - temp
+// x5 - number
+// x4 - next_j
+// x3 - next_i
+// x2 - j
+// x1 - i
+// x0 - abs address
+
+    ldr x19, =matrix
+    mul x21, x20, x20
+
+    mov x0, 0
+
+    mov x1, 0
+
+    mov x6, #2
+    udiv x2, x20, x6
+
+    mov x3, #0
+    mov x4, #0
+
+    mov x5, #1
+
+mag_square_loop:
+    cmp x5, x21
+    bgt output
+
+    mov x0, #0
+    mul x0, x1, x20
+    add x0, x0, x2
+
+    strb w5, [x19, x0]
+
+    add x5, x5, #1
+
+    sub x3, x1, #1
+    add x4, x2, #1
+
+    cmp x3, 0
+    bge next_i_ok
+
+    sub x3, x20, #1
+next_i_ok:
+
+    cmp x4, x20
+    blt next_j_ok
+
+    mov x4, #0
+
+next_j_ok:
+
+    mul x0, x3, x20
+    add x0, x0, x4
+
+    mov x6, #0
+    ldrb w6, [x19, x0]
+    cmp w6, #0
+
+    bne move_down
+
+    mov x1, x3
+    mov x2, x4
+
+    b mag_square_loop
+move_down:
+    add x1, x1, #1
+    cmp x1, x20
+    blt i_ok
+
+    mov x1, 0
+
+i_ok:
+    b mag_square_loop
+
+
+
+
 // x22 - count
 // x21 - num elements
 // x20 - matrix size
 // x19 - matrix_ptr
-good_input:
-
-
-
 output:
     mov x0, #1
     ldr x1, =msg_result
@@ -78,7 +157,7 @@ output:
     mov x8, #64
     svc #0
 
-    ldr x19, =mat
+    ldr x19, =matrix
     mul x21, x20, x20
     mov x22, #0
 output_matrix_loop:
@@ -157,4 +236,3 @@ msg_wrong_input: .asciz "Неверный ввод!\n"
 msg_wrong_input_len = . - msg_wrong_input - 1
 new_line: .asciz "\n"
 spa_char: .asciz " "
-mat: .fill 64, 1, 0
